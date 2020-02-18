489046f1f4493091ca11280eff049c450b2af60b
Geoff Norton <Geoff.Norton@wales.nhs.uk>
2020-02-18 16:41:16 +0000

    Application start-up now creates database

--- src/TrainTracker.WebAPI/Program.cs
+++ src/TrainTracker.WebAPI/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TrainTracker.WebAPI.Data;
 
 namespace TrainTracker.WebAPI
 {
@@ -14,7 +16,25 @@ namespace TrainTracker.WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<TrainTrackerContext>();
+                    context.Database.EnsureCreated(); 
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An Error occurred while creating the local database.");
+                }
+
+            }
+            host.Run();
+
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>

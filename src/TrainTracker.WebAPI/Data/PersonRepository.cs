5abe309b26be640232a508a342b26cb4d614bfc2
Geoff Norton <Geoff.Norton@wales.nhs.uk>
2020-03-18 16:01:40 +0000

    Added Repository Service

---
+++ src/TrainTracker.WebAPI/Data/PersonRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrainTracker.Core.Entities;
+using TrainTracker.Core.Services;
+
+namespace TrainTracker.WebAPI.Data
+{
+    public class PersonRepository : IDisposable, IPersonService
+    {
+        private TrainTrackerContext _context;
+        public PersonRepository(TrainTrackerContext ctx)
+        {
+            _context = ctx;
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        public void Create(Person person)
+        {
+            if (person.LastName == null) throw new ArgumentNullException("Person must have a last name");
+        }
+               
+        public Person FindById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id, Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_context != null)
+                {
+                    _context.Dispose();
+                    _context = null;
+                }
+            }
+        }
+    }
+}
---
+++ test/TrainTracker.Tests/PersonRepositoryTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TrainTracker.Core.Entities;
+using TrainTracker.WebAPI.Data;
+using Xunit;
+
+namespace TrainTracker.Tests
+{
+    public class PersonRepositoryTest
+    {
+        [Fact]
+        public void Get_RepoHasThreePeople_ReturnsThreePeople()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<TrainTrackerContext>().UseInMemoryDatabase($"PersonRepositoryDatabase{ Guid.NewGuid()}").Options;
+
+            using (var ctx = new TrainTrackerContext(options))
+            {
+                Person[] persons = new Person[] {new Person() { FirstName = "Geoff", LastName = "Norton" },
+                           new Person(){FirstName = "Sandra", LastName = "Norton" },
+                           new Person(){FirstName = "Alice", LastName = "Norton" } };
+
+                ctx.Persons.AddRange(persons);
+                ctx.SaveChanges();
+                var personRepo = new PersonRepository(ctx);
+            }
+
+            //Act
+            using (var ctx = new TrainTrackerContext(options))
+            {
+                var personRepo = new PersonRepository(ctx);
+                var results = personRepo.GetAll();
+                var actual = results.Count();
+                //Assert
+                Assert.Equal(3, actual);
+            }
+        }
+
+        [Fact]
+
+        public void Add_LastNameIsMissing_ArgumentExceptionThrown()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<TrainTrackerContext>().UseInMemoryDatabase($"PersonRepositoryDatabase{Guid.NewGuid()}").Options;
+            using (var ctx = new TrainTrackerContext(options))
+            {
+                var personRepo = new PersonRepository(ctx);
+                //Assert
+                Assert.Throws<ArgumentNullException>(()=>personRepo.Create(new Person(){ FirstName = "Geoff"}));
+            }
+        }
+    }
+}
